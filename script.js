89a49155c8c875f04ea7b8eaeb46e8e7080d706b
ejmsyr <ejmsyr@gmail.com>
2024-11-29 18:38:12 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-let Player=["ee"];
-console.log("hello");
-
-
-
-var element = document.getElementById('plength');
-element.innerHTML = "wewe";
-element.style.color = "blue";
-element.style.fontSize = "20px";
-element.style.backgroundColor = "lightgray";
-element.style.padding = "10px";
-element.style.border = "2px solid black";
-
-// document.getElementById('plength').innerHTML = "wewe";
-
-document.getElementById("plength").textContent = "Your text here";
-
 
 document.getElementById('login-btn').addEventListener('click', function () {
     const username = document.getElementById('username').value;
@@ -22,9 +5,6 @@ document.getElementById('login-btn').addEventListener('click', function () {
 
     if (password==="1"&&username!="") {
         window.location.href = 'game.html';
-        Player[Player.length]=username;
-        document.getElementById('plength').innerHTML = "wewe";
-
 
     } else {
         window.close();
