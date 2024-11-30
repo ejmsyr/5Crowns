64b25c5326b9f6fc06753948741ba31e4d081820
ejmsyr <ejmsyr@gmail.com>
2024-11-29 17:45:57 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let Player=["ee"];
+console.log("hello");
+
+
+
+var element = document.getElementById('plength');
+element.innerHTML = "wewe";
+element.style.color = "blue";
+element.style.fontSize = "20px";
+element.style.backgroundColor = "lightgray";
+element.style.padding = "10px";
+element.style.border = "2px solid black";
+
+// document.getElementById('plength').innerHTML = "wewe";
+
+document.getElementById("plength").textContent = "Your text here";
+
+
+// document.getElementById('login-btn').addEventListener('click', function () {
+//     const username = document.getElementById('username').value;
+//     const password = document.getElementById('password').value;
+
+//     if (password==="1"&&username!="") {
+//         window.location.href = 'game.html';
+//         Player[Player.length]=username;
+//         document.getElementById('plength').innerHTML = "wewe";
+
+
+//     } else {
+//         window.close();
+//     }
+// });
+
